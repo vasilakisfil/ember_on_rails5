682087473ee5316a2a8fed9710163fc829ba6ac9
Filippos Vasilakis <vasilakisfil@gmail.com>
2016-08-30 21:39:59 -0400

    Add API authentication/authorization
    wip
    
    wip
    
    wip
    
    Improve login
    
    Fix issue with currentUser loading

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,23 +2,57 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  notify: Ember.inject.service('notify'),
+
   actions: {
     createSession(session) {
       this.get('session').authenticate(
         'authenticator:devise',
         session.get('identifier'),
         session.get('password')
-      ).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
-      });
+      ).then(
+        () => {
+          this.get('notify').success('You are in!');
+          this.controllerFor('sessions.new').set('loginError', null);
+        },
+        (reason) => this.controllerFor('sessions.new').set('loginError', reason)
+      );
     },
     destroySession() {
       this.get('session').invalidate();
     },
   },
 
-  sessionInvalidated() {
-    this.transitionTo('index');
-    return true;
+  sessionObserver: Ember.observer('session.isAuthenticated', function() {
+    Ember.Logger.debug(this.get('session.isAuthenticated'));
+    if(this.get('session.isAuthenticated')) {
+      let userId = this.get('session.data.authenticated.userId');
+
+      if(this.get('store').recordIsLoaded('user', userId)) {
+        this._loadCurrentUser();
+      } else {
+        this._fetchCurrentUser();
+      }
+    } else {
+      this.set('session.currentUser', null);
+    }
+  }),
+
+  _loadCurrentUser() {
+    this.set(
+      'session.currentUser',
+      this.get('store').peekRecord(
+        'user', this.get('session.data.authenticated.userId')
+      )
+    );
+  },
+
+  _fetchCurrentUser() {
+    var userId = this.get('session.data.authenticated.userId');
+    var _this = this;
+
+    this.get('store').findRecord('user', userId).then(function(user) {
+      _this.set('session.currentUser', user);
+    });
   }
 });
